c577f3d4d7a90136fa9bb4ac3e83070d1786e723
Aymeric Richard <aymeric.richard@signature-code.com>
2023-06-21 15:44:00 +0200

    Optimize with better encapsulation and readability:
    Maximum Open Close Principle.
    Handle nullable to free the caller from all the work.

--- CK.DB.OIddict/Dapper/JsonTypeConverter.cs
+++ CK.DB.OIddict/Dapper/JsonTypeConverter.cs
@@ -3,54 +3,40 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Text.Json;
 using Newtonsoft.Json;
+using static Newtonsoft.Json.JsonConvert;
 
 namespace CK.DB.OIddict.Dapper
 {
     internal static class JsonTypeConverter
     {
-        static readonly JsonSerializerSettings _jsonSettings = new() { };
+        private static readonly JsonSerializerSettings _jsonSettings = new();
 
-        public static T FromJson<T>( string json )
+        public static T? FromJson<T>( string? json )
         {
-            if( typeof( T ) == typeof( HashSet<string> ) )
-            {
-                var collection = (T)(object)JsonConvert.DeserializeObject<HashSet<string>>( json, _jsonSettings )!;
-                return collection;
-            }
+            if( json is null ) return default;
 
-            if( typeof( T ) == typeof( HashSet<Uri> ) )
-            {
-                var collection = (T)(object)JsonConvert.DeserializeObject<HashSet<Uri>>( json, _jsonSettings )!;
-                return collection;
-            }
-
-            if( typeof( T ) == typeof( Dictionary<string, JsonElement> ) )
-            {
-                var collection = (T)(object)JsonConvert.DeserializeObject<Dictionary<string, JsonElement>>
-                ( json, _jsonSettings )!;
-                return collection;
-            }
-
-            if( typeof( T ) == typeof( Dictionary<CultureInfo, string> ) )
-            {
-                var collection = (T)(object)JsonConvert.DeserializeObject<Dictionary<CultureInfo, string>>
-                ( json, _jsonSettings )!;
-                return collection;
-            }
+            if( _deserializationMap.TryGetValue( typeof( T ), out var deserializer ) )
+                return (T)deserializer.Invoke( json );
 
             throw new NotSupportedException();
         }
 
-        public static string ToJson( HashSet<string> collection ) =>
-        JsonConvert.SerializeObject( collection, _jsonSettings );
+        private static readonly Dictionary<Type, Func<string, object>> _deserializationMap = new()
+        {
+            { typeof( HashSet<string> ), Deserialize<HashSet<string>> },
+            { typeof( HashSet<Uri> ), Deserialize<HashSet<Uri>> },
+            { typeof( Dictionary<string, JsonElement> ), Deserialize<Dictionary<string, JsonElement>> },
+            { typeof( Dictionary<CultureInfo, string> ), Deserialize<Dictionary<CultureInfo, string>> },
+        };
 
-        public static string ToJson( Dictionary<string, JsonElement> collection ) =>
-        JsonConvert.SerializeObject( collection, _jsonSettings );
+        private static T Deserialize<T>( string json ) => DeserializeObject<T>( json, _jsonSettings )!;
 
-        public static string ToJson( HashSet<Uri> collection ) =>
-        JsonConvert.SerializeObject( collection, _jsonSettings );
+        public static string? ToJson( HashSet<string>? collection ) => SerializeToJson( collection );
+        public static string? ToJson( Dictionary<string, JsonElement>? collection ) => SerializeToJson( collection );
+        public static string? ToJson( HashSet<Uri>? collection ) => SerializeToJson( collection );
+        public static string? ToJson( Dictionary<CultureInfo, string>? collection ) => SerializeToJson( collection );
 
-        public static string ToJson( Dictionary<CultureInfo, string> collection ) =>
-        JsonConvert.SerializeObject( collection, _jsonSettings );
+        private static string? SerializeToJson( object? collection ) =>
+        collection is null ? null : SerializeObject( collection, _jsonSettings );
     }
 }
