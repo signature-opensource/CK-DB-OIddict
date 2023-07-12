5b1f8f21a172ecd5726b641ecf426881cfbd8ad9
Aymeric Richard <aymeric.richard@signature-code.com>
2023-07-12 12:02:18 +0200

    Webpack now creates 2 chunks, one for the login and one for the consent:
    Both with a specific html

--- Samples/CK.DB.OIddict.DefaultServer.App/WebFrontAuth/webpack.config.js
+++ Samples/CK.DB.OIddict.DefaultServer.App/WebFrontAuth/webpack.config.js
@@ -2,18 +2,27 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './index.ts',
+  entry: {
+    index: './index.ts',
+    consent: './Authorization/index.ts'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[name].js',
     libraryTarget: 'var',
-    library: 'Sample',
+    library: ['Sample', '[name]'],
     libraryExport: 'default',
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: './index.html',
-      filename: './index.html'
+      filename: 'index.html',
+      chunks: ['index']
+    }),
+    new HtmlWebPackPlugin({
+      template: './Authorization/consent.html',
+      filename: './Authorization/Consent.html',
+      chunks: ['consent']
     }),
     new HtmlWebPackPlugin({
       template: './style.css',
