d32867cc51d800297e1bb4654ffa61676ee07aaf
Aymeric Richard <aymeric.richard@signature-code.com>
2023-08-25 19:25:08 +0200

    Fix documentation.

--- CK.DB.OIddict/Commands/ISoftUpdateApplicationCommand.cs
+++ CK.DB.OIddict/Commands/ISoftUpdateApplicationCommand.cs
@@ -7,7 +7,7 @@ namespace CK.DB.OIddict.Commands
         /// <summary>
         /// Only changed properties will be set. If a property is null it is then ignored.
         /// If you want to override everything, use <see cref="IHardUpdateApplicationCommand"/>.
-        /// Be aware that collections are overriden.
+        /// Be aware that collections are not overriden but merged.
         /// </summary>
         IApplicationPoco ApplicationPoco { get; set; }
     }
